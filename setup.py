41c2e2069d998b602e099c3566e9b4e360b9093e
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-06-03 23:56:53 +0100

    Make setup a function

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ import RPi.GPIO as GPIO
 
 import config
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
 
-for servoPIN in config.legs.values():
-    p = GPIO.PWM(servoPIN, 50)
-    p.start(2.5)
-    p.stop()
-    GPIO.cleanup()
+def centerServos():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servoPIN, GPIO.OUT)
+
+    for servoPIN in config.legs.values():
+        p = GPIO.PWM(servoPIN, 50)
+        p.start(2.5)
+        p.stop()
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    centerServos()
