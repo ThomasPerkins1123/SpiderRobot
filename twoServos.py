6eef213bd5a798c049be285de0b023962201af75
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-07-12 20:32:24 +0100

    Now two servo script uses pigpio

--- twoServos.py
+++ twoServos.py
@@ -1,44 +1,36 @@
-import RPi.GPIO as GPIO
+import pigpio
+
 import time
 
 servoPIN = 14
 servoPIN2 = 15
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
-GPIO.setup(servoPIN2, GPIO.OUT)
+pi = pigpio.pi()
 
+pi.set_mode(servoPIN, pigpio.OUTPUT)
+pi.set_mode(servoPIN2, pigpio.OUTPUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-a = GPIO.PWM(servoPIN2, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
-a.start(2.5) # Initialization
-try:
-  while True:     
-    p.ChangeDutyCycle(5)
-    a.ChangeDutyCycle(5)
+while True:     
+    pi.set_servo_pulsewidth(servoPIN, 1500)
+    pi.set_servo_pulsewidth(servoPIN2, 1500)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    a.ChangeDutyCycle(7.5)
+    pi.set_servo_pulsewidth(servoPIN, 1800)
+    pi.set_servo_pulsewidth(servoPIN2, 1800)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    a.ChangeDutyCycle(10)
+    pi.set_servo_pulsewidth(servoPIN, 2100)
+    pi.set_servo_pulsewidth(servoPIN2, 2100)
     time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    a.ChangeDutyCycle(12.5)
+    pi.set_servo_pulsewidth(servoPIN, 1800)
+    pi.set_servo_pulsewidth(servoPIN2, 1800)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    a.ChangeDutyCycle(10)
+    pi.set_servo_pulsewidth(servoPIN, 1500)
+    pi.set_servo_pulsewidth(servoPIN2, 1500)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    a.ChangeDutyCycle(7.5)
+    pi.set_servo_pulsewidth(servoPIN, 1200)
+    pi.set_servo_pulsewidth(servoPIN2, 1200)
     time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    a.ChangeDutyCycle(5)
+    pi.set_servo_pulsewidth(servoPIN, 900)
+    pi.set_servo_pulsewidth(servoPIN2, 900)
     time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    a.ChangeDutyCycle(2.5)
+    pi.set_servo_pulsewidth(servoPIN, 1200)
+    pi.set_servo_pulsewidth(servoPIN2, 1200)
     time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  a.stop()
-  GPIO.cleanup()
