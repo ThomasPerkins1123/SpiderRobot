22cd3bdefa18aa2ef92165e7c56efbb125dc3931
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-07-17 22:20:29 +0100

    Add wave method to leg

--- parts.py
+++ parts.py
@@ -1,10 +1,33 @@
+import time
 import pigpio
+
 
 pi = pigpio.pi()
 
 class Leg:
-    def __init__(self):
-        print("Setting up leg")
+    def __init__(self, pins):
+        self.hip = Joint(pins[0], [1200, 1800])
+        self.knee = Joint(pins[1], [1200, 1800])
+        self.foot = Joint(pins[2], [1200, 1800])
+
+    def wave(self):
+        self.hip.goMiddle()
+        self.knee.goMiddle()
+        self.foot.goMiddle()
+       
+        time.sleep(1)
+        self.knee.goMax()
+        self.foot.goMin()
+        time.sleep(1)
+        self.hip.goMax()
+        time.sleep(1)
+        self.hip.goMin()
+        time.sleep(1)
+
+        self.hip.goMiddle()
+        self.knee.goMiddle()
+        self.foot.goMiddle()
+
 
 class Joint:
     def __init__(self, pinNumber, rangeOfMotion):
