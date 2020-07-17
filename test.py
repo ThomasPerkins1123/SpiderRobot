551517fabef52eb25853637c41e9160c7e12d5e2
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-07-17 22:33:32 +0100

    Test script now uses two legs

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from parts import Leg
 import time
 
-legOne = Leg([18, 15, 14])
+legOne = Leg([18, 15, 14], True, True)
+legTwo = Leg([25, 24, 23], False, True)
+#legThree = Leg([18, 15, 14])
+#legFour = Leg([18, 15, 14])
 legOne.wave()
