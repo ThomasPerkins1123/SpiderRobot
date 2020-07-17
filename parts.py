f0ae095103d3af564f54567bffcfe25d32ed4963
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-07-17 22:49:13 +0100

    Adjust max of hips

--- parts.py
+++ parts.py
@@ -6,7 +6,7 @@ pi = pigpio.pi()
 
 class Leg:
     def __init__(self, pins, front, right):
-        self.hip = Joint(pins[0], [1300, 2000, 200], not front)
+        self.hip = Joint(pins[0], [1300, 2200, 400], not front)
         self.knee = Joint(pins[1], [1200, 1800], right)
         self.foot = Joint(pins[2], [1200, 1800], right)
 
