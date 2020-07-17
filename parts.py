331289423d68aa8186ada1d75b371f99f61a2227
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-07-17 23:44:11 +0100

    Hips min and max now relative to root

--- parts.py
+++ parts.py
@@ -6,9 +6,11 @@ pi = pigpio.pi()
 
 class Leg:
     def __init__(self, pins, front, right):
-        self.hip = Joint(pins[0], [1300, 2200, 400], front ^ right)
-        self.knee = Joint(pins[1], [600, 1800], right)
-        self.foot = Joint(pins[2], [800, 2500], not front)
+        self.hip = Joint(pins[0], [200, 700, 0], front ^ right)
+        self.knee = Joint(pins[1], [0, 0], right)
+        self.foot = Joint(pins[2], [0, 0], not front)
+        #self.knee = Joint(pins[1], [600, 1800], right)
+        #self.foot = Joint(pins[2], [800, 2500], not front)
 
     def wave(self):
         self.hip.goMiddle()
@@ -36,17 +38,21 @@ class Joint:
         self.pinNumber = pinNumber
         pi.set_mode(pinNumber, pigpio.OUTPUT)
         self.center = 1500
-        if not flipped:
-            self.min = rangeOfMotion[0]
-            self.max = rangeOfMotion[1]
-        else:    
-            self.min = rangeOfMotion[1]
-            self.max = rangeOfMotion[0]
         if len(rangeOfMotion) > 2:
             if not flipped:
                 self.center += rangeOfMotion[2]
             else:
                 self.center -= rangeOfMotion[2]
+
+        self.min = self.center
+        self.max = self.center
+
+        if not flipped:
+            self.min -= rangeOfMotion[0]
+            self.max += rangeOfMotion[1]
+        else:
+            self.min += rangeOfMotion[0]
+            self.max -= rangeOfMotion[1]
         self.goMiddle()
 
     def goMiddle(self):
