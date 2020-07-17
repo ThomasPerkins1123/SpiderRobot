289fd2c36edecdcafc44ac3d38e390366df0488f
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-07-18 00:03:45 +0100

    Update test script

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import time
 
 frontR = Leg([25, 24, 23], True, True)
 backR = Leg([18, 15, 14], False, True)
-frontL = Leg([12, 7, 8], True, False)
-backL = Leg([11, 9, 10], False, False)
+frontL = Leg([11, 9, 10], True, False)
+backL = Leg([12, 7, 8], False, False)
+
 frontR.wave()
+frontL.wave()
+backR.wave()
+backL.wave()
