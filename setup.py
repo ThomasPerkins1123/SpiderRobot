6a557667473b4b7ee999249ad232c3a37ec2230a
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-10-24 15:53:26 +0100

    Add some sleeps

--- setup.py
+++ setup.py
@@ -9,5 +9,7 @@ if __name__ == "__main__":
         servoPIN = int(input("Enter a servo pin:"))
         pi.set_mode(servoPIN, pigpio.OUTPUT)
         pi.set_servo_pulsewidth(servoPIN, 1800)
+        time.sleep(0.5)
         pi.set_servo_pulsewidth(servoPIN, 1500)
+        time.sleep(0.5)
         pi.set_servo_pulsewidth(servoPIN, 1200)
