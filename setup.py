fcf813a4d47b4228f1a10deac76e4b3896bda519
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-10-24 15:52:11 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ pi = pigpio.pi()
 
 if __name__ == "__main__":
     while True:
-        servoPIN = input("Enter a servo pin:")
+        servoPIN = int(input("Enter a servo pin:"))
         pi.set_mode(servoPIN, pigpio.OUTPUT)
         pi.set_servo_pulsewidth(servoPIN, 1800)
         pi.set_servo_pulsewidth(servoPIN, 1500)
