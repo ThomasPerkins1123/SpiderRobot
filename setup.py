c6c5a00110a942f22752430b73f693b311129a96
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-10-24 15:49:33 +0100

    Add a new script for geting pin numbers

--- setup.py
+++ setup.py
@@ -4,14 +4,10 @@ import config
 
 pi = pigpio.pi()
 
-
-def center_servos():
-    for servoPIN in config.legs.values():
-        if servoPIN != 0:
-            print(servoPIN)
-            pi.set_mode(servoPIN, pigpio.OUTPUT)
-            pi.set_servo_pulsewidth(servoPIN, 1500)
-
-
 if __name__ == "__main__":
-    center_servos()
+    while True:
+        servoPIN = input("Enter a servo pin:")
+        pi.set_mode(servoPIN, pigpio.OUTPUT)
+        pi.set_servo_pulsewidth(servoPIN, 1800)
+        pi.set_servo_pulsewidth(servoPIN, 1500)
+        pi.set_servo_pulsewidth(servoPIN, 1200)
